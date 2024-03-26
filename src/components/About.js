5ac30fac1ed346bd9f2bb6b53c1ebde76eb96ddf
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-03-26 16:27:01 +0330

    end of about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+
+import { about } from "../data";
+
+import { IoIosArrowDroprightCircle } from "react-icons/io";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  const { title, subtitle1, subtitle2, icon, link } = about;
 
-export default About
+  return (
+    <section className="py-[80px] md:py-[110px] lg:pt-[140px] lg:pb-[180px]">
+      <div className="container mx-auto px-[20px] lg:px-[135px]">
+        <div
+          className="section-title-group justify-start"
+          data-aos="fade-up"
+          data-aos-delay="100"
+        >
+          <img src={icon} alt="." />
+          <h3 className="h2 section-title">
+            {title} <span className="text-primary-200">.</span>
+          </h3>
+        </div>
+        <p
+          data-aos="fade-up"
+          data-aos-delay="200"
+          className="md:text-body-md mb-12"
+        >
+          {subtitle1}
+        </p>
+        <p
+          data-aos="fade-up"
+          data-aos-delay="300"
+          className="md:text-body-md mb-8"
+        >
+          {subtitle2}
+        </p>
+        <div data-aos="fade-up" data-aos-delay="400">
+          <a
+            className="link flex items-center gap-x-2 hover:gap-x-6 transition-all duration-300"
+            href="#"
+          >
+            {link}
+            <IoIosArrowDroprightCircle className="text-2xl" />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
