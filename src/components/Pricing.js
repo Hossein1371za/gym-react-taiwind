36c683dab2f71641a76881ffb014670b983ff02d
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-03-27 23:44:46 +0330

    end of pricing section

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -9,13 +9,18 @@ const Pricing = () => {
 
   return (
     <section className="section">
-      <div className="section-title-group max-w-[540px] mx-auto px-4 lg:px-0">
+      <div
+        data-aos="fade-up"
+        data-aos-offset="200"
+        data-aos-delay="200"
+        className="section-title-group max-w-[540px] mx-auto px-4 lg:px-0"
+      >
         <img src={icon} alt="." />
         <h2 className="h2">
           {title} <span className="text-primary-200">.</span>
         </h2>
       </div>
-      <PlanList plans={plans}/>
+      <PlanList plans={plans} />
     </section>
   );
 };
