e1def7e87866173208ecfe8ea561ccc19900e8fb
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-03-27 22:18:27 +0330

    Pricing section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import './App.css';
-import Aos from 'aos';
-import "aos/dist/aos.css"
+import "./App.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
-import Header from './components/Header';
-import Banner from './components/Banner';
-import About from './components/About';
-import Workouts from './components/workout/Workouts';
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import About from "./components/About";
+import Workouts from "./components/workout/Workouts";
+import Pricing from "./components/Pricing";
 
 function App() {
   Aos.init({
-    duration:2500,
-    delay:400,
-  })
+    duration: 2500,
+    delay: 400,
+  });
   return (
     <div className="max-w-[1440px] mx-auto bg-page overflow-hidden relative">
-      <Header/>
-      <Banner/>
-      <About/>
-      <Workouts/>
-      <div className='h-[4000px]'></div>
+      <Header />
+      <Banner />
+      <About />
+      <Workouts />
+      <Pricing />
+      <div className="h-[4000px]"></div>
     </div>
   );
 }
