58da6f05b5081fe8d9aad1ffb11914f34ed0a23d
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-03-29 19:23:57 +0330

    accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Accordion = ({accordion}) => {
+import { FaChevronCircleUp, FaChevronCircleDown } from "react-icons/fa";
+
+const Accordion = ({ accordion }) => {
+  const { question, answer } = accordion;
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>Accordion</div>
-  )
-}
+    <div onClick={() => setOpen(!open)} className="cursor-pointer">
+      <div className="bg-white border rounded-sm">
+        <div className="min-h-[60px] flex items-center justify-between px-[30px]">
+          <h6 className="h6">{question}</h6>
+          <div className="transition text-[20px] text-neutral-500">
+            {open ? <FaChevronCircleUp /> : <FaChevronCircleDown />}
+          </div>
+        </div>
+        <div className={`${open ? "min-h[200px] lg:min-h-[160px]" : "hidden"} flex justify-center transition-all duration-500 px-[30px]`}>
+          <div className="mt-6">{answer}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Accordion
+export default Accordion;
