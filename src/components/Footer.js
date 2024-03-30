d6f9faa4a9fe2524d189af373345466f45dc09d4
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-03-30 13:47:09 +0330

    end of footer & project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
+import { footer } from '../data'
+
 const Footer = () => {
+  const {logo,copyrightText} = footer
   return (
-    <div>Footer</div>
+    <footer className="bg-neutral-500 h-[125px] md:h-[195px] px-[20px]">
+      <div className='container m-auto h-full flex items-center justify-between md:items-end md:pb-[50px]'
+      data-aos="fade-up"
+      data-aos-delay="200">
+        <a href='/'>
+          <img src={logo} alt=''/>
+        </a>
+        <p className='text-neutral-300 text-sm'>{copyrightText}</p>
+      </div>
+    </footer>
   )
 }
 
